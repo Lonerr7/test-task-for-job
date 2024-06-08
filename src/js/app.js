be9c304fcbc5a4fe0545838f09839caa2c7983be
Ivan <ivan.karabankov@gmail.com>
2024-06-08 13:54:03 +0300

    6. Created desktop design of the carousel. Made it move on mouse drag

--- src/js/app.js
+++ src/js/app.js
@@ -3,3 +3,33 @@ import * as functions from './modules/functions.js';
 // Checking if browsers supports .webp
 functions.isWebp();
 
+// Caroursel
+const carousel = document.querySelector('.slider__carousel');
+
+let isDragging = false;
+let startX;
+let startScrollLeft;
+
+const dragStart = (e) => {
+  isDragging = true;
+  carousel.classList.add('dragging');
+
+  // Records the initial cursor and scroll position of the carousel
+  startX = e.pageX;
+  startScrollLeft = carousel.scrollLeft;
+}
+
+const dragStop = () => {
+  isDragging = false;
+  carousel.classList.remove('dragging');
+}
+
+const handleMousmove = (e) => {
+  if (!isDragging) return;
+  // Updates postion of the carousel based on the mouse move
+  carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+}
+
+carousel.addEventListener('mousedown', dragStart);
+carousel.addEventListener('mousemove', handleMousmove);
+carousel.addEventListener('mouseup', dragStop);
